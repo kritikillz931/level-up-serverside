8f86e1988c8955c307084988ce0bad8746325ff6
Justin <justinadams758@gmail.com>
2021-08-03 14:14:53 -0500

    exposing events

--- levelup/urls.py
+++ levelup/urls.py
@@ -19,11 +19,14 @@ from rest_framework import routers
 from levelupapi.views import GameTypeView
 from levelupapi.views import register_user, login_user
 from levelupapi.views import GameView
+from levelupapi.views import EventView
+
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
